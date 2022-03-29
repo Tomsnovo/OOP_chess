b95c1b44596746d6959ed788f481a4235108f80d
Tomáš Bejček <Bejcek.Tomas@stud.vassboskovice.cz>
2022-03-29 09:13:23 +0200

    Přidejte soubory projektu.

---
+++ OOP_chess/Figure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_chess
+{
+    public class Figure
+    {
+        public FigureType Type { get; set; }
+
+        public Figure()
+        {
+            
+        }
+    }
+
+
+    //výčtový datový typ - seznam hodnot 
+    public enum FigureType
+    {
+        Pawn,   //pěšec
+        Rook,   //věž
+        Knight, //jezdec
+        Bishop, //střelec
+        Queen,  //dáma
+        King,   //král
+    }
+}
