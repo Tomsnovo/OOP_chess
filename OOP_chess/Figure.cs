b2281a47ff3aa453eb5bfdaec9835ae915688eb8
Tomáš Bejček <Bejcek.Tomas@stud.vassboskovice.cz>
2022-03-29 09:40:50 +0200

    dobg

--- OOP_chess/Figure.cs
+++ OOP_chess/Figure.cs
@@ -6,11 +6,30 @@ namespace OOP_chess
 {
     public class Figure
     {
+        private List<string> numbers = new List<string>() { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        private string position;
+        
         public FigureType Type { get; set; }
+        public string Position
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+
+                }
+                else if (numbers.Contains(value.Substring(1,1)))
+                {
+
+                }
+            }
+
+            get => position;
+        }
 
         public Figure()
         {
-            
+            Position = "A2";
         }
     }
 
