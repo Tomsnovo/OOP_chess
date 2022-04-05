04733e773cf166b0513f2c20ac2997379b5de28b
Tomáš Bejček <Bejcek.Tomas@stud.vassboskovice.cz>
2022-04-05 09:36:00 +0200

    gd

--- OOP_chess/Figure.cs
+++ OOP_chess/Figure.cs
@@ -6,31 +6,58 @@ namespace OOP_chess
 {
     public class Figure
     {
-        private List<string> numbers = new List<string>() { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-        private string position;
+        private List<string> numbers = new List<string>() { "1", "2", "3", "4", "5", "6", "7", "8" };
+        private List<string> letters = new List<string>() { "A", "B", "C", "D", "E", "F", "G", "H" };
+        private string position= "";
         
-        public FigureType Type { get; set; }
+        public FigureType Type { get; }
         public string Position
+
+       
         {
             set
             {
                 if (string.IsNullOrEmpty(value))
                 {
+                    position = value;
+                }
+                else if (value.Length == 2) 
+                { 
+
+                    if (letters.Contains(value.Substring(0, 1).ToUpper()) && numbers.Contains(value.Substring(1, 1)))
+                    {
+                        position = value;
+                    }
+                    else
+                    {
+                        throw new Exception("Figure position value has to be valid chess position!");
+                    }
 
                 }
-                else if (numbers.Contains(value.Substring(1,1)))
+                else
                 {
-
+                    throw new Exception("Figure position value has to be 2 letters valid chess position!");
                 }
             }
 
             get => position;
         }
+        public FigureColor Color { get; }
 
-        public Figure()
+        public Figure(FigureType type, FigureColor color)
         {
-            Position = "A2";
+            Type = type;
+            Color = color;
         }
+
+        public Figure(FigureType type, string position, FigureColor color)
+        {
+            Type = type;
+            Position = position;
+            Color = color;
+        }
+
+        
     }
 
 
@@ -43,5 +70,11 @@ namespace OOP_chess
         Bishop, //střelec
         Queen,  //dáma
         King,   //král
+    }
+
+    public enum FigureColor
+    {
+        White,
+        Black
     }
 }
